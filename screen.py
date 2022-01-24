4b5fbab53d9f540129d24e26aa95ce165cbcd780
Martin <74269598+martabal@users.noreply.github.com>
2022-01-24 18:12:35 +0100

    Update screen.py

--- screen.py
+++ screen.py
@@ -16,32 +16,25 @@ import subprocess
 
 
 def get_cpu_temp():
-    """
-    Obtains the current value of the CPU temperature.
-    :returns: Current value of the CPU temperature if successful, zero value otherwise.
-    :rtype: float
-    """
-    # Initialize the result.
+
     result = 0.0
-    # The first line in this file holds the CPU temperature as an integer times 1000.
-    # Read the first line and remove the newline character at the end of the string.
     if os.path.isfile('/sys/class/thermal/thermal_zone0/temp'):
         with open('/sys/class/thermal/thermal_zone0/temp') as f:
             line = f.readline().strip()
-        # Test if the string is an integer as expected.
         if line.isdigit():
-            # Convert the string with the CPU temperature to a float in degrees Celsius.
             result = float(line) / 1000
-    # Give the result back to the caller.
     return result
 
+
 button1 = 40
-button3 = 37
+button2 = 37
 screenday = False
 cpu_temp = str(round(float(get_cpu_temp()),2))
 cpu_percent = str(psutil.cpu_percent(4))
 screennb = 0
 photoresistorpin = 36
+humidity,temp = adht.read_retry(adht.DHT22, 4)
+
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -51,9 +44,11 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
+
 def setup1():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(button1, GPIO.IN)
+
 
 def loop1():
     global screenday
@@ -69,18 +64,18 @@ def loop1():
                 screenday = True
 
 
-
-def setup3():
+def setup2():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(button3, GPIO.IN)
+    GPIO.setup(button2, GPIO.IN)
 
-def loop3():
+
+def loop2():
     global screennb
     while True:
-        button_state = GPIO.input(button3)
+        button_state = GPIO.input(button2)
         if  button_state == False:
 
-            while GPIO.input(button3) == False:
+            while GPIO.input(button2) == False:
                 sleep(0.2)
             if screennb == 0 :
                 screennb = 1
@@ -91,7 +86,8 @@ def loop3():
             elif screennb == 3:
                 screennb = 0
 
-def setup4():
+
+def setup3():
 
     GPIO.setmode(GPIO.BOARD)
     #Output on the pin for
@@ -101,7 +97,6 @@ def setup4():
 
     #Change the pin back to input
     GPIO.setup(photoresistorpin, GPIO.IN)
-
 
 
 def photoresistor():
@@ -116,6 +111,15 @@ def photoresistor():
             light = True
             sleep(1)
 
+
+def temphumid():
+    global humidity
+    global temp
+    while True:
+        humidity,temp = adht.read_retry(adht.DHT22, 4)
+        sleep(20)
+
+
 def getcpu():
     global cpu_temp
     global cpu_percent
@@ -124,67 +128,39 @@ def getcpu():
         cpu_percent = str(psutil.cpu_percent(4))
 
 
-
 def main():
     onetime = True
     serial = i2c(port=1, address=0x3C)
     # substitute ssd1331(...) or sh1106(...) below if using that device
-    h,t = adht.read_retry(adht.DHT22, 4)
     ni.ifaddresses('eth0')
     IP_addres = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-    now = datetime.now()
-    time1 = now + timedelta(seconds=2)
-    time2 = now + timedelta(seconds=30)
     device = sh1106(serial)
-    docker = ["YOUR_DOCKER_CONTAINERS"]
+    docker = ["YOUR_DOCKER_CONTAINER"]
 
-    while 1==1:
-
-
-
-
-
+    while True:
         if(light == True or screenday == True):
-            now = datetime.now()
-            if(time2 < now):
-                now = datetime.now()
-                time2 = now + timedelta(seconds=30)
-                h,t = adht.read_retry(adht.DHT22, 4)
-
-
             if(screennb == 0):
                 with canvas(device) as draw:
-
                     now = datetime.now()
                     time1 = now + timedelta(seconds=2)
                     draw.text((0, 0)," CPU : " +  cpu_percent +"%, "  + cpu_temp+"°C  " , fill="white")
-                    draw.text((0, 50)," Humidity : " +  str(round(float(h),2))+"%", fill="white")
-                    draw.text((0, 40)," Temp : " +  str(round(float(t),2))+"°C", fill="white")
+                    draw.text((0, 50)," Humidity : " +  str(round(float(humidity),2))+"%", fill="white")
+                    draw.text((0, 40)," Temp : " +  str(round(float(temp),2))+"°C", fill="white")
                     draw.text((0, 20)," IP : " +  str(IP_addres), fill="white")
                     draw.text((0,10), " Mem : " + str(psutil.virtual_memory()[2]) +"%", fill="white")
 
             elif(screennb == 1):
-
-
                 with canvas(device) as draw:
-
-
-
-
                     draw.text((0,0)," Screen : " + str(screenday), fill="white")
 
             elif(screennb == 2):
-
-
                 with canvas(device) as draw:
                     draw.text((0,0)," Docker started : " , fill="white")
                     line = 20
                     for i in range(4) :
                         if is_container_running(docker[i]) == True :
-
                             draw.text((0,line), docker[i], fill = "white")
                             line = line + 10
-
 
             elif(screennb == 3):
                 if(len(docker) > 4) :
@@ -196,15 +172,8 @@ def main():
                         line = 0
                         for i in range(4, number) :
                             if is_container_running(docker[i]) == True :
-
                                 draw.text((0,line), docker[i], fill = "white")
                                 line = line + 10
-
-
-
-
-
-
 
         else:
             with canvas(device) as draw:
@@ -228,20 +197,10 @@ def testtime():
 
 
 
-
 def is_container_running(container_name: str) -> Optional[bool]:
-    """Verify the status of a container by it's name
 
-    :param container_name: the name of the container
-    :return: boolean or None
-    """
     RUNNING = "running"
-    # Connect to Docker using the default socket or the configuration
-    # in your environment
     docker_client = docker.from_env()
-    # Or give configuration
-    # docker_socket = "unix://var/run/docker.sock"
-    # docker_client = docker.DockerClient(docker_socket)
 
     try:
         container = docker_client.containers.get(container_name)
@@ -256,18 +215,20 @@ if __name__ == "__main__":
     #container_name = "pihole"
     #result = is_container_running(container_name)
     #print(result)
-    setup4()
-    setup3()
     setup1()
+    setup2()
+    setup3()
 
-
-    p4 = Thread( target=photoresistor)
-    p2 = Thread( target=getcpu)
-    p1 = Thread( target=loop1)
-    p3 = Thread( target=loop3)
     p = Thread( target=main)
-    p4.start()
-    p3.start()
-    p2.start()
-    p1.start()
+    p1 = Thread( target=loop1)
+    p2 = Thread( target=getcpu)
+    p3 = Thread( target=loop2)
+    p4 = Thread( target=photoresistor)
+    p5 = Thread(target = temphumid)
+
     p.start()
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+    p5.start()
